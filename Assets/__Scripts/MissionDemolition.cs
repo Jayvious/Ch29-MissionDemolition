ee032946afd556ece7a47ccbc13e69b05ebbca37
Jayvious <JayviousWilliams@gmail.com>
2019-10-03 20:14:56 -0400

    Update MissionDemolition.cs

--- Assets/__Scripts/MissionDemolition.cs
+++ Assets/__Scripts/MissionDemolition.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 // TODO: You must set the values for the enum
 public enum GameMode
 {
+    idle,
+    playing,
+    levelEnd
 
 
 }
@@ -14,6 +17,22 @@ public enum GameMode
 
 // TODO: implement the MissionDemolition script
 public class MissionDemolition : MonoBehaviour {
+    static private MissionDemolition S;
+
+    [Header("Set in Inpector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public Text uitButton;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Set Dynamically")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public GameObject castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
 
 
 
@@ -23,17 +42,47 @@ public class MissionDemolition : MonoBehaviour {
 	// Use this for initialization
 	void Start ()
     {
+        S = this;
+
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
+
+
 
     }
 	
 	void StartLevel()
     {
+       if(castle != null)
+        {
+            Destroy(castle);
+        }
+
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
+        foreach(GameObject pTemp in gos)
+        {
+            Destroy(pTemp);
+        }
+
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+        shotsTaken = 0;
+
+        SwitchView("Show Both");
+        ProjectileLine.S.Clear();
+
+        Goal.goalMet = false;
+        UpdateGUI();
+
+        mode = GameMode.playing;
 
  
     }
 
     void UpdateGUI()
     {
+
 
     }
 
